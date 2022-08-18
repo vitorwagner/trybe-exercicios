b6b752ec9911b90824756ae7b6713b0b9d10c98c
Vitor Wagner <vitormwagner@gmail.com>
2022-08-18 17:21:41 -0300

    Programa 2 e 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -7,3 +7,31 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+// Programa 2
+const c = 30;
+const d = 10;
+let result;
+
+if (c > d) {
+  result = c;
+} else {
+  result = d;
+}
+
+console.log(result);
+
+// Programa 3
+const e = 30;
+const f = 10;
+const g = 20;
+
+if (e > f && e > g) {
+  result = e;
+} else if (f > e && f > g) {
+  result = f;
+} else {
+  result = g;
+}
+
+console.log(result);
